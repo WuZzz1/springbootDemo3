ea9f31b7ecc748c888f53b6ff856ca36d02f593d
WuZzz1 <653767813@qq.com>
2021-06-17 17:51:55 +0800

    测试1

---
+++ src/main/java/com/cau/springboot/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.cau.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/springboot/say")
+    public @ResponseBody String say(){
+        return "Hello SpringBoot!"+" Hello GitHub！";
+    }
+
+}
---
+++ src/test/java/com/cau/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cau.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
